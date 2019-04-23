c0f7ab429faf61ca9c3470d593291add201ddfef
Chirayu Desai <chirayudesai1@gmail.com>
2019-04-23 15:59:24 +0530

    Initial import of game files
    * New C++ Project, Side Scroller
    * Mobile, Scalable, No starter content

---
+++ Source/Orexis/OrexisGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OrexisGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AOrexisGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AOrexisGameMode();
+};
+
+
+
